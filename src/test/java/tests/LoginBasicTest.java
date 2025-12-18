95e3feea17b578e7a173a78cad69bf8d38066241
Salah Eddine FARES <salahefs@gmail.com>
2025-12-18 16:24:21 +0100

    Refactor project structure and add ExtentReports for test reporting.

--- src/test/java/tests/LoginBasicTest.java
+++ src/test/java/tests/LoginBasicTest.java
@@ -1,62 +1,59 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Dimension;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.Assert;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import pages.LoginPage;
+import utils.ExtentManager;
 
 public class LoginBasicTest {
 
     private WebDriver driver;
+    private ExtentReports extent;
+    private ExtentTest test;
 
     @BeforeTest
     public void setUp() {
-        // Automatically downloads and sets up ChromeDriver for Mac M2
-        WebDriverManager.chromedriver().setup();
+        extent = ExtentManager.getInstance();
+        test = extent.createTest("LoginBasicTest");
 
-        // Create ChromeDriver instance
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
     }
 
     @AfterTest
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        if (driver != null) driver.quit();
+        extent.flush(); // save report
     }
 
     @Test
-    public void login() {
-        System.out.println("0. Start");
+    public void login() throws InterruptedException {
+        test.log(Status.INFO, "Opening saucedemo.com");
 
-        System.out.println("1. Open target page");
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().setSize(new Dimension(1350, 637));
 
-        System.out.println("2. Insert username and password");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
 
-        System.out.println(" 2.1 Insert username");
-        driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
+        test.log(Status.INFO, "Login submitted");
 
-        System.out.println(" 2.2 Insert password");
-        driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
-
-        System.out.println("3. Click submit to perform login");
-        driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
-
-        System.out.println("4. Verify login has been successfully executed");
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
         Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
 
-        // Pause for 2 seconds to show the logged-in page
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("5. End");
+        test.log(Status.PASS, "Login verified successfully");
     }
 }
